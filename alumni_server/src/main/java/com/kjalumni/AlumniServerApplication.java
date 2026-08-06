8ac386e2d79446e6580ae008d10a86b0f0eadf23
omrakibe <rakibeom30@gmail.com>
2026-08-06 17:49:23 +0530

    first commit

---
+++ alumni_server/src/main/java/com/kjalumni/AlumniServerApplication.java
@@ -0,0 +1,15 @@
+package com.kjalumni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlumniServerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(AlumniServerApplication.class, args);
+    }
+
+}
