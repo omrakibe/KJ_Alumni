d82d7d18faf2a916d4828811872d318f77db15a3
omrakibe <rakibeom30@gmail.com>
2026-08-14 19:33:17 +0530

    Revert "Route Protection(Security)"
    This reverts commit 4cb1df5fdb7808ede1ba52669d2a02bf687d481a.

--- alumni_client/src/routes/AppRoutes.jsx
+++ alumni_client/src/routes/AppRoutes.jsx
@@ -1,124 +1,38 @@
-import {
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/public/Home";
-
 import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 import VerifyEmail from "../pages/auth/VerifyEmail";
 import ForgotPassword from "../pages/auth/ForgotPassword";
 import ResetPassword from "../pages/auth/ResetPassword";
-
 import AlumniDashboard from "../pages/alumni/AlumniDashboard";
 import AdminDashboard from "../pages/admin/AdminDashboard";
-
-import ProtectedRoute from "../component/auth/ProtectedRoute";
 
 function AppRoutes() {
   return (
     <Routes>
 
-      {/* =========================================
-          PUBLIC
-      ========================================== */}
+      {/* Public */}
+      <Route path="/" element={<Home />} />
 
-      <Route
-        path="/"
-        element={<Home />}
-      />
-
-
-      {/* =========================================
-          AUTHENTICATION
-      ========================================== */}
-
-      <Route
-        path="/login"
-        element={<Login />}
-      />
-
-      <Route
-        path="/register"
-        element={<Register />}
-      />
-
-      <Route
-        path="/verify-email"
-        element={<VerifyEmail />}
-      />
-
-      <Route
-        path="/forgot-password"
-        element={<ForgotPassword />}
-      />
-
-      <Route
-        path="/reset-password"
-        element={<ResetPassword />}
-      />
-
-
-      {/* =========================================
-          ALUMNI
-      ========================================== */}
-
-      <Route
-        path="/alumni"
-        element={
-          <ProtectedRoute allowedRole="ALUMNI">
-            <AlumniDashboard />
-          </ProtectedRoute>
-        }
-      />
-
+      {/* Authentication */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/verify-email" element={<VerifyEmail />} />
+      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/reset-password" element={<ResetPassword />} />
+      
+      {/* Alumni */}
       <Route
         path="/alumni/dashboard"
-        element={
-          <ProtectedRoute allowedRole="ALUMNI">
-            <AlumniDashboard />
-          </ProtectedRoute>
-        }
+        element={<AlumniDashboard />}
       />
 
-
-      {/* =========================================
-          ADMIN
-      ========================================== */}
-
-      <Route
-        path="/admin"
-        element={
-          <ProtectedRoute allowedRole="ADMIN">
-            <AdminDashboard />
-          </ProtectedRoute>
-        }
-      />
-
+      {/* Admin */}
       <Route
         path="/admin/dashboard"
-        element={
-          <ProtectedRoute allowedRole="ADMIN">
-            <AdminDashboard />
-          </ProtectedRoute>
-        }
-      />
-
-
-      {/* =========================================
-          UNKNOWN ROUTES
-      ========================================== */}
-
-      <Route
-        path="*"
-        element={
-          <Navigate
-            to="/login"
-            replace
-          />
-        }
+        element={<AdminDashboard />}
       />
 
     </Routes>
