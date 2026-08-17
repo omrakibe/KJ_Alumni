8b9ed26d248db662e94a48edbac662955f7a96bf
PrathmeshPalkar <prathmeshpalkar2@gmail.com>
2026-08-17 08:02:10 +0530

    build alumni aprovel page

--- alumni_client/src/routes/AppRoutes.jsx
+++ alumni_client/src/routes/AppRoutes.jsx
@@ -1,42 +1,90 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/public/Home";
+
 import Login from "../pages/auth/Login";
 import Register from "../pages/auth/Register";
 import VerifyEmail from "../pages/auth/VerifyEmail";
 import ForgotPassword from "../pages/auth/ForgotPassword";
 import ResetPassword from "../pages/auth/ResetPassword";
+
 import AlumniDashboard from "../pages/alumni/AlumniDashboard";
+
 import AdminDashboard from "../pages/admin/AdminDashboard";
+import AlumniManagement from "../pages/admin/AlumniManagement";
+
 
 function AppRoutes() {
   return (
     <Routes>
 
-      {/* Public */}
-      <Route path="/" element={<Home />} />
+      {/* =========================
+          PUBLIC
+      ========================== */}
 
-      {/* Authentication */}
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/verify-email" element={<VerifyEmail />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
-      <Route path="/reset-password" element={<ResetPassword />} />
-      
-      {/* Alumni */}
+      <Route
+        path="/"
+        element={<Home />}
+      />
+
+
+      {/* =========================
+          AUTHENTICATION
+      ========================== */}
+
+      <Route
+        path="/login"
+        element={<Login />}
+      />
+
+      <Route
+        path="/register"
+        element={<Register />}
+      />
+
+      <Route
+        path="/verify-email"
+        element={<VerifyEmail />}
+      />
+
+      <Route
+        path="/forgot-password"
+        element={<ForgotPassword />}
+      />
+
+      <Route
+        path="/reset-password"
+        element={<ResetPassword />}
+      />
+
+
+      {/* =========================
+          ALUMNI
+      ========================== */}
+
       <Route
         path="/alumni/dashboard"
         element={<AlumniDashboard />}
       />
 
-      {/* Admin */}
+
+      {/* =========================
+          ADMIN
+      ========================== */}
+
       <Route
         path="/admin/dashboard"
         element={<AdminDashboard />}
       />
 
+      <Route
+        path="/admin/alumni"
+        element={<AlumniManagement />}
+      />
+
     </Routes>
   );
 }
+
 
 export default AppRoutes;
